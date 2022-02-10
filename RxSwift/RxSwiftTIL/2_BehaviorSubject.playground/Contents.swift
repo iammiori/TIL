24115c90b3f278462567f33c226ac13d007884d9
iammiori <immioriii@gmail.com>
2022-02-10 17:40:28 +0900

    [220210] feat - 정확한 비교를 위해 err, complete 이벤트를 추가

--- RxSwift/RxSwiftTIL/2_BehaviorSubject.playground/Contents.swift
+++ RxSwift/RxSwiftTIL/2_BehaviorSubject.playground/Contents.swift
@@ -2,7 +2,7 @@ import RxSwift
 
 //MARK: BehaviorSubject
 enum Err : Error {
-    case error
+    case err
 }
 
 let disposeBag = DisposeBag()
@@ -12,7 +12,33 @@ let bs = BehaviorSubject<String>(value: "a")
 
 bs.subscribe { print("observer1", $0) }
     .disposed(by: disposeBag)
-// output : observer1 next(a)
-bs.onNext("b")
-// output : observer1 next(b)
 
+bs.onNext("b")
+bs.onNext("c")
+bs.subscribe { print("observer2", $0) }
+.disposed(by: disposeBag)
+bs.onNext("d")
+//bs.onCompleted()
+bs.onError(Err.err)
+bs.subscribe { print("observer3", $0) }
+.disposed(by: disposeBag)
+/*
+ output:
+ observer1 next(a)
+ observer1 next(b)
+ observer1 next(c)
+ observer2 next(c)
+ observer1 next(d)
+ observer2 next(d)
+ observer3 next(d)
+ (if err)
+ observer1 next(a)
+ observer1 next(b)
+ observer1 next(c)
+ observer2 next(c)
+ observer1 next(d)
+ observer2 next(d)
+ observer1 error(err)
+ observer2 error(err)
+ observer3 error(err)
+ */
