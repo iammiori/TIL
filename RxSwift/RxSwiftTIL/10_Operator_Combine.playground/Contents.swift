4b244c28bb1e5fdde02b0eee463497edd8139e72
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-02-17 22:22:35 +0900

    [220217] feat : combine- startwith 추가 (#23)

--- RxSwift/RxSwiftTIL/10_Operator_Combine.playground/Contents.swift
+++ RxSwift/RxSwiftTIL/10_Operator_Combine.playground/Contents.swift
@@ -1,3 +1,16 @@
-import UIKit
+import RxSwift
 
-var greeting = "Hello, playground"
+let disposeBag = DisposeBag()
+
+let arr : [Int] = [1,2,3]
+
+Observable.from(arr)
+    .startWith(0)
+    .subscribe { print("startWith : \($0)")}
+    .disposed(by: disposeBag)
+
+Observable.from(arr)
+    .startWith(-2,-1,0)
+    .startWith(-3)
+    .subscribe { print("startWith 2 : \($0)")}
+    .disposed(by: disposeBag)
