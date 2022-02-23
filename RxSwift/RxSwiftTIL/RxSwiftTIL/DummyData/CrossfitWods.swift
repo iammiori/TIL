970589e93ba9d5e798aee8de242581f12e699f08
iammiori <immioriii@gmail.com>
2022-02-23 23:03:25 +0900

    [220223] modify - 데이터 순서 변경

--- RxSwift/RxSwiftTIL/RxSwiftTIL/DummyData/CrossfitWods.swift
+++ RxSwift/RxSwiftTIL/RxSwiftTIL/DummyData/CrossfitWods.swift
@@ -9,7 +9,7 @@ import Foundation
 
 let CrossfitWods : [CrossfitMovements] = [
     CrossfitMovements(category: .girlsName, name: "KAREN", movements: "For Time\n150 wall balls", level: "rx : 20lb"),
+    CrossfitMovements(category: .girlsName, name: "CINDY", movements: "AMRAP in 20 min\n5 Pull Ups\n10 Push Ups\n15 Air Squats", level: "rx : 20lb"),
     CrossfitMovements(category: .hero, name: "RANDY", movements: "For Time\n75 Power Snatch", level: "rx : 75lb/55lb"),
-    CrossfitMovements(category: .hero, name: "DT", movements: "For Time\n12 DeadLift\n9 Hang Power Clean\n6 Push Jerk", level: "rx : 155lb/105lb"),
-    CrossfitMovements(category: .girlsName, name: "CINDY", movements: "AMRAP in 20 min\n5 Pull Ups\n10 Push Ups\n15 Air Squats", level: "rx : 20lb")
+    CrossfitMovements(category: .hero, name: "DT", movements: "For Time\n12 DeadLift\n9 Hang Power Clean\n6 Push Jerk", level: "rx : 155lb/105lb")
     ]
