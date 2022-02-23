6a4b7f2ce58a9749bbbdcbe40b2e41253b58001b
iammiori <immioriii@gmail.com>
2022-02-23 16:08:40 +0900

    [220223] feat - 구조체 따르는 더미데이터 제작

---
+++ RxSwift/RxSwiftTIL/RxSwiftTIL/DummyData/CrossfitWods.swift
@@ -0,0 +1,13 @@
+//
+//  CrossfitWods.swift
+//  RxSwiftTIL
+//
+//  Created by miori Lee on 2022/02/23.
+//
+
+import Foundation
+
+let CrossfitWods : [CrossfitMovements] = [
+    CrossfitMovements(category: .girlsName, name: "KAREN", movements: "For Time\n150 wall balls", level: "rx : 20lb"),
+    CrossfitMovements(category: .hero, name: "RANDY", movements: "For Time\n75reps Power Snatch", level: "rx : 75lb")
+    ]
