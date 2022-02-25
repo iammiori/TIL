ad69da2ed2ea2e85ccae91131577178c553b2ec1
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-02-25 20:51:37 +0900

    [220225] Separate extension (#37)
    * [220225] delete : extension 따로 파일 만들거라, 코드 삭제
    
    * [220225] feat ; UITextField + Rx Extension 구현

--- RxSwift/RxSwiftTIL/RxSwiftTIL/DriverVC.swift
+++ RxSwift/RxSwiftTIL/RxSwiftTIL/DriverVC.swift
@@ -98,24 +98,3 @@ extension DriverVC {
         return email.contains("@")
     }
 }
-
-extension Reactive where Base : UITextField {
-    var beginEditing : ControlEvent<Void> {
-        return controlEvent(.editingDidBegin)
-    }
-    var endEditing : ControlEvent<Void> {
-        return controlEvent(.editingDidEnd)
-    }
-    var borderColorWidth : Binder<ColorWidth?> {
-//        return Binder(self.base) { textfield, colorWidth in
-//            textfield.layer.borderColor = colorWidth?.color.cgColor
-//            textfield.layer.borderWidth = CGFloat( colorWidth?.width ?? 0.5)
-//        }
-        //단축인자
-        return Binder(self.base) {
-            $0.layer.borderColor = $1?.color.cgColor
-            $0.layer.borderWidth = CGFloat($1?.width ?? 0.5)
-        }
-        
-    }
-}
---
+++ RxSwift/RxSwiftTIL/RxSwiftTIL/Extension/UITextField+Rx.swift
@@ -0,0 +1,33 @@
+//
+//  UITextField+Rx.swift
+//  RxSwiftTIL
+//
+//  Created by miori Lee on 2022/02/25.
+//
+
+import RxSwift
+import RxCocoa
+import UIKit
+
+
+extension Reactive where Base : UITextField {
+    var beginEditing : ControlEvent<Void> {
+        return controlEvent(.editingDidBegin)
+    }
+    var endEditing : ControlEvent<Void> {
+        return controlEvent(.editingDidEnd)
+    }
+    var borderColorWidth : Binder<ColorWidth?> {
+//        return Binder(self.base) { textfield, colorWidth in
+//            textfield.layer.borderColor = colorWidth?.color.cgColor
+//            textfield.layer.borderWidth = CGFloat( colorWidth?.width ?? 0.5)
+//        }
+        //단축인자
+        return Binder(self.base) {
+            $0.layer.borderColor = $1?.color.cgColor
+            $0.layer.borderWidth = CGFloat($1?.width ?? 0.5)
+        }
+        
+    }
+}
+
