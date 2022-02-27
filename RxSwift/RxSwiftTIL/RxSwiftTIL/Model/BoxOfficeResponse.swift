4d85eedc6edbf3d253af694127182984a15bb363
iammiori <immioriii@gmail.com>
2022-02-27 23:00:20 +0900

    [229227] fetat : BoxOffice response model 구현

---
+++ RxSwift/RxSwiftTIL/RxSwiftTIL/Model/BoxOfficeResponse.swift
@@ -0,0 +1,20 @@
+//
+//  BoxOfficeResponse.swift
+//  RxSwiftTIL
+//
+//  Created by miori Lee on 2022/02/27.
+//
+
+import Foundation
+
+struct BoxOfficeResponse : Decodable {
+    let boxOfficeResult : BoxOfficeDetail
+}
+
+struct BoxOfficeDetail : Decodable {
+    let weeklyBoxOfficeList : [BoxOfficeList]
+}
+
+struct BoxOfficeList : Decodable {
+    let movieNm : String
+}
