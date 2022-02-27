92845cc7feab34e7f9facb2e3e76af1d34d740d8
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-02-27 23:03:23 +0900

    [220227] Rx+url session 모든 과정 (#40)
    * [229227] fetat : BoxOffice response model 구현
    
    * [220227] feat : boxoffice url 반환해주는 함수 구현
    
    * [220227] feat : rx + urlsession 네트워킹 하여 디코드 하는 과정
    
    * [220227] feat : BoxofficeVC에서 네트워크 함수 호출

---
+++ RxSwift/RxSwiftTIL/RxSwiftTIL/Network/BoxOfficeAPI.swift
@@ -0,0 +1,27 @@
+//
+//  BoxOfficeAPI.swift
+//  RxSwiftTIL
+//
+//  Created by miori Lee on 2022/02/27.
+//
+
+import Foundation
+
+struct BoxOfficeAPI {
+    static let scheme = "https"
+    static let host = "kobis.or.kr"
+    static let path = "/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json"
+    
+    func getBoxOffice() -> URLComponents {
+        var components = URLComponents()
+        components.scheme = BoxOfficeAPI.scheme
+        components.host = BoxOfficeAPI.host
+        components.path = BoxOfficeAPI.path
+        
+        components.queryItems = [
+            URLQueryItem(name: "key", value: Secret.openAPIKEY),
+        URLQueryItem(name: "targetDt", value: "20220217")
+        ]
+        return components
+    }
+}
