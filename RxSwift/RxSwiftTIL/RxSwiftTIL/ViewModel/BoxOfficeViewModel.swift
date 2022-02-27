c40682c1bb87200bd753efb5355c2ba27b75607e
iammiori <immioriii@gmail.com>
2022-02-28 05:12:54 +0900

    [220228] feat : ViewModel  구현

---
+++ RxSwift/RxSwiftTIL/RxSwiftTIL/ViewModel/BoxOfficeViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  BoxOfficeViewModel.swift
+//  RxSwiftTIL
+//
+//  Created by miori Lee on 2022/02/28.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+struct BoxOfficeViewModel {
+    let disposeBag = DisposeBag()
+    // 셀에 뿌릴값
+    let detailListCellData : Driver<[BoxOfficeList]>
+    // 받아온값
+    let apiData = PublishSubject<[BoxOfficeList]>()
+    
+    init(_ networkModel : BoxOfficeNetwork = BoxOfficeNetwork()) {
+        
+        //네트워크값 observable로
+        let boxOfficeResult = networkModel.getBoxOffice()
+            .asObservable()
+            .share()
+        
+        // success일때 value 꺼내고 nil 제거
+        let boxOfficeResultValue = boxOfficeResult
+            .compactMap { data -> BoxOfficeResponse? in
+                guard case let .success(value) = data
+                else {
+                    return nil
+                }
+                return value
+            }
+        
+        // respose 형태 중 weeklyBoxOfficeList로 map
+        boxOfficeResultValue
+            .map { $0.boxOfficeResult.weeklyBoxOfficeList }
+            .bind(to: apiData)
+            .disposed(by: disposeBag)
+
+        self.detailListCellData = apiData
+            .asDriver(onErrorJustReturn: [])
+    }
+}
