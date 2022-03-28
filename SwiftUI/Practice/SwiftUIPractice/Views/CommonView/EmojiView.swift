82be0d015689aa259d12101ad44e6ee37d433913
iammiori <immioriii@gmail.com>
2022-03-28 23:43:32 +0900

    [style] 파일 그룹화, 그룹명 Views로 변경

---
+++ SwiftUI/Practice/SwiftUIPractice/Views/CommonView/EmojiView.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiView.swift
+//  SwiftUIPractice
+//
+//  Created by miori Lee on 2022/03/24.
+//
+
+import SwiftUI
+
+struct EmojiView: View {
+    let emoji : String
+    let emojiArr : [String] = ["🏋🏻‍♀️","🤸‍♀️","🥊","🤺","🍔","🍕","🌮","🍣"]
+    
+    init(number: Int) {
+        self.emoji = emojiArr[number % 8]
+        print("EmojiView \(number) initialized")
+    }
+    
+    var body: some View {
+        Text(emoji)
+            .font(.system(size: 50))
+            .frame(width: 150, height: 150)
+            .background(.gray)
+            .clipShape(Circle())
+    }
+}
+
+struct NumberView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiView(number: 8)
+    }
+}
