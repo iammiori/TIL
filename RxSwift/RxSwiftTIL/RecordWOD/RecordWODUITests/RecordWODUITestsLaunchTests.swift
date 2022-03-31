0a835484264997629c0d192de22cef31df7ad6e0
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-04-01 02:33:50 +0900

    [feat] test  파일 커밋 (#70)

---
+++ RxSwift/RxSwiftTIL/RecordWOD/RecordWODUITests/RecordWODUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RecordWODUITestsLaunchTests.swift
+//  RecordWODUITests
+//
+//  Created by miori Lee on 2022/03/12.
+//
+
+import XCTest
+
+class RecordWODUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
