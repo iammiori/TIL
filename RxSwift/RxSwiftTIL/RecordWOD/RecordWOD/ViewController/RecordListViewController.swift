801b411e1c437ebc4e3bce45d0172ea41940bfe9
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-04-01 02:42:31 +0900

    Record list view (ViewModel - View Binding) (#72)
    * [feat] RecordListViewModel 구현
    
    * [feat] 뷰모델과 바인딩 함수 작성

--- RxSwift/RxSwiftTIL/RecordWOD/RecordWOD/ViewController/RecordListViewController.swift
+++ RxSwift/RxSwiftTIL/RecordWOD/RecordWOD/ViewController/RecordListViewController.swift
@@ -13,16 +13,26 @@ import RxCocoa
 
 class RecordListViewController : UIViewController , ViewModelBindableType {
     
+    let disposeBag = DisposeBag()
     var viewModel: RecordListViewModel!
     
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: disposeBag)
         
+        viewModel.recordList
+            .bind(to: tableView.rx.items(cellIdentifier: RecordListTableViewCell.registerID, cellType: RecordListTableViewCell.self)) { [weak self] row, element, cell in
+                cell.setData(element)
+            }
+            .disposed(by: disposeBag)
+        
+        addBtn.rx.action = viewModel.makeCreateAction()
     }
     
 
     let tableView = UITableView()
-    
-    let disposeBag = DisposeBag()
+    var addBtn = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
         attribute()
@@ -42,13 +52,14 @@ extension RecordListViewController {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.title = "운동 기록 🏋🏻‍♀️"
         self.view.backgroundColor = .white
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        addBtn.setImage(UIImage(systemName: "plus"), for: .normal)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: addBtn)
         tableView.register(RecordListTableViewCell.self, forCellReuseIdentifier: RecordListTableViewCell.registerID)
     }
     
     private func layout() {
         self.view.addSubview(tableView)
-        
+        [tableView,addBtn].forEach {  self.view.addSubview($0) }
         tableView.snp.makeConstraints {
             $0.centerX.leading.trailing.equalToSuperview()
             $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(8)
