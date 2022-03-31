1b33ad9b735d5530df34335df3bb2d9f84bbe570
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-04-01 02:44:50 +0900

    Edit record view (ViewModel - View) (#73)
    * [feat] EditRecordViewModel -  새 기록을 저장/취소할 수 있게끔
    
    * [feat] EditRecrod UI 구현 및 뷰 모델과 바인딩

--- RxSwift/RxSwiftTIL/RecordWOD/RecordWOD/ViewController/EditRecordViewController.swift
+++ RxSwift/RxSwiftTIL/RecordWOD/RecordWOD/ViewController/EditRecordViewController.swift
@@ -6,13 +6,34 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
 
 class EditRecordViewController: UIViewController, ViewModelBindableType {
     
+    let textView = UITextView()
+    var cancelBtn = UIButton()
+    var saveBtn = UIButton()
     var viewModel: EditRecordViewModel!
+    let disposeBag = DisposeBag()
 
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: disposeBag)
         
+        viewModel.initalText
+            .drive(textView.rx.text)
+            .disposed(by: disposeBag)
+        
+        cancelBtn.rx.action = viewModel.cancelAction
+        
+        saveBtn.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withLatestFrom(textView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: disposeBag)
     }
     
     override func viewDidLoad() {
@@ -20,6 +41,20 @@ class EditRecordViewController: UIViewController, ViewModelBindableType {
 
         attribute()
         layout()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        textView.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if textView.isFirstResponder {
+            textView.resignFirstResponder()
+        }
     }
 
 }
@@ -29,9 +64,21 @@ extension EditRecordViewController {
         self.navigationItem.largeTitleDisplayMode = .never
         self.title = "메모 수정"
         self.view.backgroundColor = .white
+        [cancelBtn,saveBtn].forEach { $0.setTitleColor(.systemBlue, for: .normal)}
+        cancelBtn.setTitle("Cancel", for: .normal)
+        saveBtn.setTitle("Save", for: .normal)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: saveBtn)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: cancelBtn)
+        
+        textView.autocorrectionType = .no
+        textView.autocapitalizationType = .none
     }
     
     private func layout() {
+        [textView,cancelBtn,saveBtn].forEach { self.view.addSubview($0)}
         
+        textView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
--- RxSwift/RxSwiftTIL/RecordWOD/RecordWOD/ViewModel/EditRecordViewModel.swift
+++ RxSwift/RxSwiftTIL/RecordWOD/RecordWOD/ViewModel/EditRecordViewModel.swift
@@ -6,7 +6,39 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class EditRecordViewModel {
+class EditRecordViewModel : CommonViewModel {
+    //기록을 저장하는 속성
+    // 새로운 기록 : nil
+    //  편집 : string
+    private let content : String?
+    // 뷰에 바인딩 할수 있도록 driver
+    var initalText : Driver<String?> {
+        return Observable.just(content).asDriver(onErrorJustReturn: nil)
+    }
     
+    let saveAction : Action<String, Void>
+    let cancelAction : CocoaAction
+    init(title : String, content : String? = nil, sceneCoordinator : SceneCoordinatorType, storage : RecordStorageType, saveAction : Action<String, Void>? = nil, cancelAction : CocoaAction? = nil) {
+        self.content = content
+        self.saveAction = Action<String,Void> { input in
+            if let action = saveAction {
+                action.execute(input)
+            }
+            return sceneCoordinator.close(animated: true).asObservable().map { _ in }
+        }
+        
+        self.cancelAction = CocoaAction {
+            if let action = cancelAction {
+                action.execute(())
+            }
+            
+            return sceneCoordinator.close(animated: true).asObservable().map { _ in }
+        }
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
 }
