1e885043c6881d1f79ee6e072f4759774d897824
iammiori <immioriii@gmail.com>
2022-03-31 23:42:04 +0900

    [feat] group에 대해 알아보고 group 속성 modifier로 한꺼번에 바꾸기

---
+++ SwiftUI/Practice/SwiftUIPractice/Views/ViewGroup/ViewGrouping.swift
@@ -0,0 +1,46 @@
+//
+//  ViewGrouping.swift
+//  SwiftUIPractice
+//
+//  Created by miori Lee on 2022/03/27.
+//
+
+import SwiftUI
+
+struct ViewGrouping: View {
+    var body: some View {
+        VStack {
+            Group {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+            .font(.title2) //공통속성 한번에 뚝딱
+            
+            Group {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+            .padding()
+            VStack {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+   
+            }
+            .padding()
+        }
+    }
+}
+
+struct ViewGrouping_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewGrouping()
+    }
+}
