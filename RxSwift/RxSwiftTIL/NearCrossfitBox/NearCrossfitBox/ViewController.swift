36f4a35012489c6d84d1c848dc2a7f982335cb18
iammiori <immioriii@gmail.com>
2022-04-06 01:37:25 +0900

    [feat] 주변 크로스핏 박스 찾기 프로젝트 셋팅

---
+++ RxSwift/RxSwiftTIL/NearCrossfitBox/NearCrossfitBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearCrossfitBox
+//
+//  Created by miori Lee on 2022/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
