34c0518ed1f7388e8d81f83f90c2cc33ddf3d0d4
iammiori <mioriii@naver.com>
2022-05-02 23:55:38 +0900

    feat - RxDataSources 쓰기위해 코드 수정

--- RxSwift/RxSwiftTIL/RecordWOD/RecordWOD/ViewModel/RecordListViewModel.swift
+++ RxSwift/RxSwiftTIL/RecordWOD/RecordWOD/ViewModel/RecordListViewModel.swift
@@ -9,13 +9,35 @@ import Foundation
 import RxSwift
 import RxCocoa
 import Action
+import RxDataSources
+
+//MARK: SectionModel (rxdatasource 쓰기위해)
+
+typealias RecordSctionModel = AnimatableSectionModel<Int, Record>
 
 class RecordListViewModel : CommonViewModel {
     // 테이블뷰와 바인딩 할 수 있는 속성
-    var recordList : Observable<[Record]> {
+//    var recordList : Observable<[Record]> {
+//        return storage.readRecord()
+//    }
+    var recordList : Observable<[RecordSctionModel]> {
         return storage.readRecord()
     }
     
+    //MARK: Rx - 테이블뷰 바인딩에 사용한 데이터 소스 속성
+    let datasoruce : RxTableViewSectionedAnimatedDataSource<RecordSctionModel> = {
+        let ds = RxTableViewSectionedAnimatedDataSource<RecordSctionModel> { dataSource, tableView, indexPath, record in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: RecordListTableViewCell.registerID, for: indexPath) as? RecordListTableViewCell else { return UITableViewCell() }
+            //let cell = tableView.dequeueReusableCell(withIdentifier: RecordListTableViewCell.registerID, for: indexPath)
+            cell.mainLabel.text = record.record
+            //cell.textLabel?.text = record.record
+            return cell
+        }
+        ds.canEditRowAtIndexPath = { _, _ in return true }
+        return ds
+    }()
+    
+
     // + 버튼 눌렀을 때 새메모 모달
     func makeCreateAction() -> CocoaAction {
         // 새 기록 방출하는 옵저버블
@@ -58,6 +80,13 @@ class RecordListViewModel : CommonViewModel {
             
             return self.sceneCoordinator.transition(to: detailScene, using: .push, animated: true)
                 .asObservable()
+                .map { _ in }
+        }
+    }()
+    
+    lazy var deleteAction : Action<Record, Void> =  {
+        return Action { record in
+            return self.storage.deleteRecord(record: record)
                 .map { _ in }
         }
     }()
