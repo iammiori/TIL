05476bd6c91d056ddbdbf147504ecf567d76fe69
iammiori <mioriii@naver.com>
2022-05-02 23:54:46 +0900

    feat - RxDataSources 사용위해  코드 수정

--- RxSwift/RxSwiftTIL/RecordWOD/RecordWOD/Model/Record.swift
+++ RxSwift/RxSwiftTIL/RecordWOD/RecordWOD/Model/Record.swift
@@ -6,12 +6,16 @@
 //
 
 import Foundation
+import RxDataSources
+import CoreData
+import RxCoreData
 
-struct Record : Equatable {
+//RxDataSources 사용위해 IdentifiableType 추가
+struct Record : Equatable, IdentifiableType {
     var record : String
     var recordDate : Date
     var identity : String
-    
+    // String은 Hashable 프로토콜을 채용한 형식
     init(record : String, recordDate : Date = Date()) {
         self.record = record
         self.recordDate = recordDate
@@ -23,4 +27,37 @@ struct Record : Equatable {
         self = pastData
         self.record = newRecord
     }
+}
+
+extension Record : Persistable {
+    //entityName 반환
+    static var entityName: String {
+        return "Record"
+    }
+    // id필드
+    static var primaryAttributeName: String {
+        return "identity"
+    }
+    
+    init(entity : NSManagedObject) {
+        record = entity.value(forKey: "content") as! String
+        recordDate = entity.value(forKey: "insertDate") as! Date
+        identity = "\(recordDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    func update(_ entity: NSManagedObject) {
+        entity.setValue(record, forKey: "content")
+        entity.setValue(recordDate, forKey: "insertDate")
+        entity.setValue("\(recordDate.timeIntervalSinceReferenceDate)", forKey: "identity")
+        
+        //rxcoredate context 자동 저장
+        //coredata 는 직접 구현해야함
+        do {
+            try entity.managedObjectContext?.save()
+        } catch {
+            print(error)
+        }
+    }
+    
+    
 }
